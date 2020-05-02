d77945acf542cdf6b4bf4e0d8dc4967a5e66058a
Jacob Faulk <jacob5567@gmail.com>
2020-05-02 15:53:09 -0700

    parsing subdirectories

--- updateREADME.py
+++ updateREADME.py
@@ -1,10 +1,21 @@
+# This program is run before each push to update the README
+
 import os
 
 
 def main():
     dir_names = [i for i in next(os.walk('.'))[1] if i[0] != '.']
-    # TODO use dictionary to hold directory names and files beneath them
+    file_dict = []
+    index = 0
+    for i in dir_names:
+        file_dict.append([])
+        for j in os.listdir("./" + i):
+            file_dict[index].append(j[:-3]) # -3 to remove the .md
+        index += 1
 
+    # Write to file
+    with open("README.md", 'w') as f:
+        pass
 
 if __name__ == "__main__":
     main()
