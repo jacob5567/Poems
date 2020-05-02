f00180abf329b1aac32008fe8ba9410477cb4887
Jacob Faulk <jacob5567@gmail.com>
2020-05-02 13:58:00 -0700

    updated readme automation to get directory names

--- updateREADME.py
+++ updateREADME.py
@@ -1,4 +1,10 @@
-print("it worked")
-f = open("testfile.txt", "w+")
-f.write("test")
-f.close()
+import os
+
+
+def main():
+    dir_names = [i for i in next(os.walk('.'))[1] if i[0] != '.']
+    # TODO use dictionary to hold directory names and files beneath them
+
+
+if __name__ == "__main__":
+    main()
